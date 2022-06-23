9c79a3eeeb31a39254244ca4a843258684b2fc14
Dan Yu Liang <liangy3@oregonstate.edu>
2022-06-22 20:52:33 -0400

    Generates AQI index values for a certain date+location+distance radius

--- us_air_quality.py
+++ us_air_quality.py
@@ -4,4 +4,27 @@
 import requests
 import json
 
-# air_API = requests.get('')
+# payload = {} air_API = requests.post('https://www.airnowapi.org/aq/forecast/zipCode/?format=text/csv&zipCode=02148
+# &date=2022-06-21&distance=25&API_KEY=E7C77666-4F3C-4065-B861-F5CCCC069C00', data=payload)
+
+print("Welcome to the Air Quality Monitoring program. If you would like to obtain weather data, please enter the "
+      "following:")
+
+user_zip = input("What is your zip code? ")
+user_date = input("When is the date? ")
+user_distance = input("How far are you searching for the radius to your location? (mi) ")
+link = f"https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode={user_zip}&date={user_date}&distance={int(user_distance)}&API_KEY=E7C77666-4F3C-4065-B861-F5CCCC069C00"
+
+air_API = requests.get(link)
+aqi_list = []     # will be used to store the AQI values
+
+# converts the air_API text form into JSON, this is now a list that looks like this [{"DateIssue":...},
+# {"DateIssue":...}, ...]
+air_JSON = json.loads(air_API.text)
+
+# Iterate through every dict in the air_JSON list, grab the value to the key you want, we want AQI, so access it
+# directly via dict["AQI"]
+for dict in air_JSON:
+      aqi_list.append(dict["AQI"])
+
+print(aqi_list)
