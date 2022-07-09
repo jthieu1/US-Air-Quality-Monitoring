dd5f1a974b62df6fa41d8c2d57adb6a3e30c5cca
Dan Yu Liang <liangy3@oregonstate.edu>
2022-07-09 15:54:04 -0400

    Validates zipcode

--- us_air_quality.py
+++ us_air_quality.py
@@ -3,46 +3,45 @@
 
 import requests
 import json
+import zipcodes
 
 # payload = {} air_API = requests.post('https://www.airnowapi.org/aq/forecast/zipCode/?format=text/csv&zipCode=02148
 # &date=2022-06-21&distance=25&API_KEY=E7C77666-4F3C-4065-B861-F5CCCC069C00', data=payload)
 
 def pull_data():
+    print("Welcome to the Air Quality Monitoring program. If you would like to obtain weather data, please enter the "
+          "following:")
 
-      print("Welcome to the Air Quality Monitoring program. If you would like to obtain weather data, please enter the "
-            "following:")
+    user_zip = input("What is your zip code? ")
 
-      user_zip = input("What is your zip code? ")
-      try:
-            user_zip = int(user_zip)
-      except ValueError:
-            print("Please enter a valid zip code.")
-            pass
-      user_date = input("When is the date? ")
-      # try:
-            # user_date = valid date from api 
-      user_distance = input("How far are you searching for the radius to your location? (mi) ")
-      try: 
-            user_distance = int(user_distance)
-      except ValueError:
-            print("Please enter a valid value for miles.")
-            pass
+    while not zipcodes.is_real(user_zip):
+        user_zip = input("What is your zip code? ")
 
-      link = f"https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode={user_zip}&date={user_date}&distance={int(user_distance)}&API_KEY=2D61DE65-ECB2-4E5E-B2F0-46CB789F0C84"
+    user_date = input("When is the date? ")
+    # try:
+    # user_date = valid date from api
+    user_distance = input("How far are you searching for the radius to your location? (mi) ")
+    try:
+        user_distance = int(user_distance)
+    except ValueError:
+        print("Please enter a valid value for miles.")
+        pass
 
-      air_API = requests.get(link)
-      aqi_list = []     # will be used to store the AQI values
+    link = f"https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode={user_zip}&date={user_date}&distance={int(user_distance)}&API_KEY=2D61DE65-ECB2-4E5E-B2F0-46CB789F0C84"
 
-      # converts the air_API text form into JSON, this is now a list that looks like this [{"DateIssue":...},
-      # {"DateIssue":...}, ...]
-      air_JSON = json.loads(air_API.text)
+    air_API = requests.get(link)
+    aqi_list = []  # will be used to store the AQI values
 
-      # Iterate through every dict in the air_JSON list, grab the value to the key you want, we want AQI, so access it
-      # directly via dict["AQI"]
-      for dict in air_JSON:
-            aqi_list.append(dict["AQI"])
+    # converts the air_API text form into JSON, this is now a list that looks like this [{"DateIssue":...},
+    # {"DateIssue":...}, ...]
+    air_JSON = json.loads(air_API.text)
 
-      print(aqi_list)
+    # Iterate through every dict in the air_JSON list, grab the value to the key you want, we want AQI, so access it
+    # directly via dict["AQI"]
+    for dict in air_JSON:
+        aqi_list.append(dict["AQI"])
+
+    print(aqi_list)
 
 
 pull_data()
